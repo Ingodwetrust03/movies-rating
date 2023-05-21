aa7f4c9b21200897d55b2a67e97a20ced80fd772
svetlana18055 <svetlana18055@yandex.ru>
2023-05-21 19:43:02 +0300

    правки

--- rating/src/App.tsx
+++ rating/src/App.tsx
@@ -4,21 +4,17 @@ import Stars from "./components/Stars";
 
 
 
-
-
 function App() {
-
     const counter: countStars = {
         count: 0
     }
 
-  return (
+    return (
     <>
         <h1>Рейтинг</h1>
-
-        {(counter.count == null || counter.count == undefined || counter.count < 1 || counter.count > 5) ? "Отсутствует" : <Stars count={counter}/>}
+        {(counter.count == null || false || counter.count < 1 || counter.count > 5) ? "Отсутствует" : <Stars count={counter}/>}
     </>
-  )
+    )
 }
 
 export default App
